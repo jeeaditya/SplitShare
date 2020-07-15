f6028f51cf3ff7a4503d130b48404ed7c72f1b83
Aditya Jee <jeeadityajha1@gmail.com>
2020-07-15 13:48:37 +0530

    Added Routes & Setup Express

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to SplitShare' }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/transactions', require('./routes/transactions'));
+
 app.listen(PORT, () => console.log(`Server Started On port ${PORT}`));
